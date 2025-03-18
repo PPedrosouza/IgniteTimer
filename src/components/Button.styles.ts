df72cc76b79c7bdff778a5a563356cb9a115e787
Pedro Souza <pedro.olegario23@gmail.com>
2025-03-18 07:27:47 -0300

    feat: added eslint fix

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,16 +1,9 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 type ButtomVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
 interface ButtonContainerProps {
     variant?: ButtomVariant;
-}
-
-const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
@@ -20,11 +13,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     border: 0;
     margin: 8px;
 
-    background-color: ${props => props.theme.primary};
+    background-color: ${props => props.theme['green-500']};
     color: ${props => props.theme.white};
-    /* ${props => {
-        return css`
-            background-color: ${buttonVariants[props.variant]}
-        `
-    }} */
 `
