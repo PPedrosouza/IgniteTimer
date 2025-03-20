aba2bb7946244f4da135691f7b8c49bbe1c2ac75
Pedro Souza <pedro.olegario23@gmail.com>
2025-03-19 22:14:14 -0300

    feat: added list suggestion

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,10 +16,24 @@ export function Home() {
             <form>
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+                    <TaskInput id="task" list='task-suggestion' placeholder="Dê um nome para o seu projeto" />
+
+                    <datalist id='task-suggestion'>
+                        <option value="Projeto 1"></option>
+                        <option value="Projeto 4"></option>
+                        <option value="Projeto 3"></option>
+                        <option value="Banana"></option>
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
-                    <MinutesAmountInput type="number" id="minutesAmount" placeholder="00"/>
+                    <MinutesAmountInput
+                        type="number"
+                        id="minutesAmount"
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
 
                     <span>minutos.</span>
                 </FormContainer>
