ea3b10c9dd42a906012694628c378ef0971214e4
Pedro Souza <pedro.olegario23@gmail.com>
2025-04-22 22:00:06 -0300

    feat: add reset from zod into component not in context

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -66,8 +66,6 @@ export function CyclesContextProvider({ children } : CyclesContexProviderProps) 
         setCycles((state) => [...state, newCycle])
         setActiveCycleId(id)
         setAmountSecondsPassed(0)
-
-        // reset();
     }
 
     function interruptCurrentCycle() {
