603609c9a1536ed55e31a22c3db5366e435cc222
Pedro Souza <pedro.olegario23@gmail.com>
2025-04-22 22:12:11 -0300

    feat: formatted start date

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,6 @@
 import { useContext } from "react";
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from "date-fns/locale/pt-BR";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 import { CyclesContext } from "../../contexts/CyclesContext";
 
@@ -25,21 +27,26 @@ export function History() {
                                 <tr key={cycle.id}>
                                     <td>{cycle.task}</td>
                                     <td>{cycle.minutesAmount} minutos</td>
-                                    <td>{cycle.startDate.toISOString()}</td>
+                                    <td>{formatDistanceToNow(cycle.startDate, {
+                                        addSuffix: true,
+                                        locale: ptBR
+                                    })}</td>
                                     <td>
                                         {cycle.finishedDate && <Status statuscolor="green">Concluído</Status>}
 
                                         {cycle.interruptedDate && <Status statuscolor="red">Interrompido</Status>}
 
-                                        {(!cycle.finishedDate) && (!cycle.interruptedDate) && <Status statuscolor="yellow">Em andamento</Status>}
-                                    
-                                </td>
+                                        {(!cycle.finishedDate) && (!cycle.interruptedDate) &&
+                                            <Status statuscolor="yellow">Em andamento</Status>
+                                        }
+
+                                    </td>
                                 </tr>
-                    )
+                            )
                         })}
-                </tbody>
-            </table>
-        </HistoryList>
+                    </tbody>
+                </table>
+            </HistoryList>
         </HistoryContainer >
     )
 }
